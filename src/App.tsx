7f2cb84978d0375a4b8ddf0babd64cb917c8f665
fangsong-cellink <fangsong.long@cellink.com>
2026-05-18 15:33:54 +0200

    fix 1

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to Tauri + React fix1</h1>
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
