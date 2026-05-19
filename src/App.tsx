96659b92536f7fc0a32f17a41325615b5e2605c4
fangsong-cellink <fangsong.long@cellink.com>
2026-05-19 09:34:03 +0200

    yyy

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React next version XXX breaking XXX WWW AAA WWaaaW</h1>
+      <h1>Welcome to Tauri + React next version XXX breaking XXX WWW AAA WWaaaW xxx</h1>
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
